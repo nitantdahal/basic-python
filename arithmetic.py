1223c1abed43426a28ff5c82137b6e730a6a9f8d
nsdhl <dhlnischal10@gmail.com>
2025-05-22 22:52:28 +0545

    feat: Operators in python

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+x = 5
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+
---
+++ assignment.py
@@ -0,0 +1,29 @@
+x = 5
+
+x += 3
+
+x -= 4
+
+print(x)
+
+x *= 4
+
+print(x)
+
+x /= 5
+x %= 4
+x //= 5
+x **= 2
+
+print(x)
+
+x = 2
+
+x &= 4
+x |= 3
+x ^= 5
+x >>= 3
+x <<= 2
+
+print(x := 3)
+print(x)
