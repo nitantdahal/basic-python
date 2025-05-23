f9407d217fefcc3b5ce90ee7f29d4bc7a9d1134e
nsdhl <dhlnischal10@gmail.com>
2025-05-23 13:57:05 +0545

    Basic python

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,7 +1,8 @@
 P = float(input("Enter a Principal Amount: "))
-T = float(input("Enter a Time: "))
-R = float(input("Enter a Rate: "))
+T = int(input("Enter a Time: "))
+R = int(input("Enter a Rate: "))
 
 SI = P*T*R/100
 
-print("Simple interest is: ", SI)
+print("Simple interest is: ", int(SI))
+print("Simple interest is: ", float(SI))
