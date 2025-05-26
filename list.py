d5c9a2368057ea619159e8f62570da2199defa4e
nsdhl <nitantdahal123@gmail.com>
2025-05-26 22:00:16 +0545

    Built-in data type: List

---
+++ list.py
@@ -0,0 +1,11 @@
+fruitname = ["Cherry","Banana","Apple"]
+print(fruitname)
+
+fruitname = ["Cherry","Banana","Apple"]
+print(len(fruitname))
+
+fruitname = (("Cherry","Banana","Apple"))
+print(fruitname)
+
+fruitname = ["Cherry","Banana","Apple"]
+print(type(fruitname))
