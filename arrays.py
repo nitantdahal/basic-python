9fc57cc50b677291f7450ed699671f847ac2168d
nsdhl <nitantdahal123@gmail.com>
2025-06-11 20:02:53 +0545

    Array in Python

---
+++ arrays.py
@@ -0,0 +1,29 @@
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+x = games[4]
+print(x)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+games[3] = "Carsrace"
+games[2] = "KuliDanda"
+print(games)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+x = len(games)
+print(x)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+for l in games:
+    print(l)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+games.append("Running")
+print(games)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+games.pop(2)
+print(games)
+
+games = ["Cricket", "Football", "Basketball", "Hockey", "Volleyball"]
+games.remove("Cricket")
+print(games)
+
---
+++ globalvar.py
@@ -0,0 +1,7 @@
+def greet():
+    global name
+    name = "Nitant"  # Local variable
+    print("Hello", name)
+
+greet()
+print(name)  
